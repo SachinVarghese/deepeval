fdd5a15a57cfcb1de750c71ce331b4593ad5c671
Sachin Varghese <sachin.mathew31@gmail.com>
2025-03-02 17:44:33 -0500

    Batch evaluation for GSM8K benchmark
    Signed-off-by: Sachin Varghese <sachin.mathew31@gmail.com>

--- deepeval/benchmarks/gsm8k/gsm8k.py
+++ deepeval/benchmarks/gsm8k/gsm8k.py
@@ -6,6 +6,7 @@ from deepeval.dataset import Golden
 from deepeval.benchmarks.base_benchmark import DeepEvalBaseBenchmark
 from deepeval.models import DeepEvalBaseLLM
 from deepeval.benchmarks.gsm8k.template import GSM8KTemplate
+from deepeval.benchmarks.utils import should_use_batch
 from deepeval.benchmarks.schema import NumberSchema
 from deepeval.telemetry import capture_benchmark_run
 
@@ -39,31 +40,63 @@ class GSM8K(DeepEvalBaseBenchmark):
         else:
             self.confinement_instructions = confinement_instructions
 
-    def evaluate(self, model: DeepEvalBaseLLM) -> Dict:
+    def evaluate(
+        self, model: DeepEvalBaseLLM, batch_size: Optional[int] = None
+    ) -> Dict:
         with capture_benchmark_run("GSM8K", len(self.tasks)):
             overall_correct_predictions = 0
             overall_total_predictions = self.n_problems
             predictions_row = []
+            use_batch = should_use_batch(model, batch_size)
 
             # Solving each problem
             goldens = self.load_benchmark_dataset()[: self.n_problems]
-            for idx, golden in enumerate(
-                tqdm(goldens, desc=f"Processing {self.n_problems} problems")
-            ):
-                prediction, score = self.predict(model, golden).values()
-                if score:
-                    overall_correct_predictions += 1
-                predictions_row.append(
-                    (golden.input, prediction, golden.expected_output, score)
-                )
-                if self.verbose_mode:
-                    self.print_verbose_logs(
-                        idx,
-                        golden.input,
-                        golden.expected_output,
-                        prediction,
-                        score,
+            if use_batch:
+                for i in tqdm(
+                    range(0, len(goldens), batch_size),
+                    desc=f"Batch Processing {self.n_problems} problems (batch_size={batch_size})",
+                ):
+                    goldens_batch = goldens[i : i + batch_size]
+                    batch_predictions = self.batch_predict(model, goldens_batch)
+                    for golden, prediction_dict in zip(
+                        goldens_batch, batch_predictions
+                    ):
+                        prediction = prediction_dict["prediction"]
+                        score = prediction_dict["score"]
+                        if score:
+                            overall_correct_predictions += 1
+                        predictions_row.append(
+                            (
+                                golden.input,
+                                prediction,
+                                golden.expected_output,
+                                score,
+                            )
+                        )
+
+            else:
+                for idx, golden in enumerate(
+                    tqdm(goldens, desc=f"Processing {self.n_problems} problems")
+                ):
+                    prediction, score = self.predict(model, golden).values()
+                    if score:
+                        overall_correct_predictions += 1
+                    predictions_row.append(
+                        (
+                            golden.input,
+                            prediction,
+                            golden.expected_output,
+                            score,
+                        )
                     )
+                    if self.verbose_mode:
+                        self.print_verbose_logs(
+                            idx,
+                            golden.input,
+                            golden.expected_output,
+                            prediction,
+                            score,
+                        )
 
             # Calculate overall accuracy
             overall_accuracy = (
@@ -111,6 +144,58 @@ class GSM8K(DeepEvalBaseBenchmark):
         )
 
         return {"prediction": prediction, "score": score}
+
+    def batch_predict(
+        self, model: DeepEvalBaseLLM, goldens: List[Golden]
+    ) -> List[Dict]:
+        assert (
+            self.shots_dataset != None
+        ), "Example dataset is empty. Call load_benchmark."
+        prompts = []
+        for golden in goldens:
+            prompt: dict = GSM8KTemplate.generate_output(
+                train_set=self.shots_dataset,
+                input=golden.input,
+                n_shots=self.n_shots,
+                enable_cot=self.enable_cot,
+            )
+            prompts.append(prompt)
+
+        # Enforced model generation
+        try:
+            responses: List = model.batch_generate(
+                prompts=prompts, schemas=[NumberSchema for i in prompts]
+            )
+            predictions = [res.answer for res in responses]
+        except TypeError:
+            prompts = [
+                prompt + f"\n\n{self.confinement_instructions}"
+                for prompt in prompts
+            ]
+            predictions = model.batch_generate(prompts)
+            predictions = [str(pred) for pred in predictions]
+
+        if len(predictions) is not len(goldens):
+            raise ValueError(
+                "Custom `batch_generate` method did not return the same number of generations as the number of prompts."
+            )
+
+        res = []
+        for i in range(len(predictions)):
+            prediction = predictions[i]
+            # For native models, shouldn't happen but just in case
+            if isinstance(prediction, tuple):
+                prediction = prediction[0]
+            prediction = str(prediction)
+            golden = goldens[i]
+
+            # Define Metric
+            score = self.scorer.exact_match_score(
+                golden.expected_output, prediction
+            )
+            res.append({"prediction": prediction, "score": score})
+
+        return res
 
     def load_benchmark_dataset(self) -> List[Golden]:
         from datasets import load_dataset
