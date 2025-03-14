feabc5a5915144a74b0452e8a07d9f2eeff8e951
Sachin Varghese <sachin.mathew31@gmail.com>
2025-03-13 22:58:09 -0400

    simplified extension
    Signed-off-by: Sachin Varghese <sachin.mathew31@gmail.com>

--- deepeval/benchmarks/gsm8k/gsm8k.py
+++ deepeval/benchmarks/gsm8k/gsm8k.py
@@ -134,13 +134,14 @@ class GSM8K(DeepEvalBaseBenchmark):
                 prompt=prompt, schema=NumberSchema
             )
             prediction = str(res.answer)
-        except TypeError:
+        except (AttributeError, TypeError):
             prompt += f"\n\n{self.confinement_instructions}"
             prediction = model.generate(prompt)
-            prediction = str(prediction)
+
         # For native models, shouldn't happen but just in case
-        # if isinstance(prediction, tuple):
-        #     prediction = prediction[0]
+        if isinstance(prediction, tuple):
+            prediction = prediction[0]
+        prediction = str(prediction)
 
         score = self.scorer.exact_match_score(
             golden.expected_output, prediction
@@ -171,7 +172,7 @@ class GSM8K(DeepEvalBaseBenchmark):
                 prompts=prompts, schema=NumberSchema
             )
             predictions = [str(res.answer) for res in responses]
-        except TypeError:
+        except (AttributeError, TypeError):
             prompts = [
                 prompt + f"\n\n{self.confinement_instructions}"
                 for prompt in prompts
@@ -187,8 +188,8 @@ class GSM8K(DeepEvalBaseBenchmark):
         for i in range(len(predictions)):
             prediction = predictions[i]
             # For native models, shouldn't happen but just in case
-            # if isinstance(prediction, tuple):
-            #     prediction = prediction[0]
+            if isinstance(prediction, tuple):
+                prediction = prediction[0]
             prediction = str(prediction)
             golden = goldens[i]
 
